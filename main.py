eb12b720d60b7613a197ae958169c7aaa737c244
Dmitriy Safiullin <156095519+dmitriysafiullin@users.noreply.github.com>
2024-10-24 11:17:22 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,74 @@
+import http.client
+import json
+
+
+def challenge_1():
+    """Hint: I would like to get read I think"""
+    try {
+      File myObj = new File("filename.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
+
+def challenge_2(data):
+    """thiH: tnih a uoy evig lliw siht ebyaM"""
+    data = read_data()
+    return data
+
+
+def challenge_3(data):
+    """Hint: We are too low, we need to go higher up, just in case."""
+    for data in data:
+        return data
+
+
+def send_it(name, number, api_key):
+    """If you've got it then fill in your name and phone number then send it!"""
+    host = "demo.lime-crm.com"
+    endpoint = "/ctf_challenge/api/v1/limeobject/winner/"
+    body = json.dumps({"name": name, "phone": number})
+    method = "POST"
+
+    headers = {"x-api-key": api_key, "Content-Type": "application/json"}
+
+    conn = http.client.HTTPSConnection(host)
+
+    conn.request(method, endpoint, headers=headers, body=body)
+
+    response = conn.getresponse()
+    body = response.read()
+
+    try:
+        body = json.loads(body)
+    except Exception:
+        pass
+
+    print("\nResponse:\n")
+    print(f"Status code: {response.status}\n")
+    print(f"Headers: {response.headers}")
+    print(f"Body: {body}")
+
+    if response.status and response.status > 199 and response.status < 300:
+        print("\nCongratulations! Your name has been added to the list of winners")
+
+
+if __name__ == "__main__":
+    data_1 = challenge_1()
+
+    data_2 = challenge_2(data_1)
+
+    api_key = challenge_3(data_2)
+
+    # If you think you've got it then send it. Just fill in your name and number
+    send_it(
+        "YOUR_NAME_HERE",
+        "12345678",
+        api_key,
+    )
---
+++ readme.md
@@ -0,0 +1,17 @@
+##### Instructions
+
+1. Clone this repository to your local machine
+
+2. Solve all the challenges using the hints in the comments. Your goal is to deobfuscate the obfuscated_key file and send an authenticated API request to the endpoint.
+
+3. If you think you have solved it, run the file using Python!
+
+   - `python3 ./main.py`
+
+     OR
+
+   - Run button (Arrow) in vscode
+
+4. If the text "Congratulations! Your name has been added to the list of winners" is printed, it means you have solved the challenges and been registered as a winner. Check the winners table to confirm this.
+
+5. Profit???
